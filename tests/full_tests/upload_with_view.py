468d49ec1b2b5af3a363ead00811af5ef34dc4d4
Matthew Hamilton <mwhamilton6@gmail.com>
2021-06-12 23:14:08 -0500

    Updated some more tests

--- tests/full_tests/upload_with_view.py
+++ tests/full_tests/upload_with_view.py
@@ -11,29 +11,30 @@ with base_utilities.change_directory():
         aws_creds = json.load(f)
 today = datetime.datetime.today()
 today_date = today.date()
-df_int = pandas.DataFrame([{"a": 1}, {"a": 2}, {"a": 3}])
-df_float = pandas.DataFrame([{"a": 1.0}, {"a": 2.1}, {"a": 3.0}])
-df_dt = pandas.DataFrame([{"a": today}, {"a": today}, {"a": None}])
-df_date = pandas.DataFrame([{"a": today_date}, {"a": today_date}, {"a": None}])
-df_bool = pandas.DataFrame([{"a": True}, {"a": False}, {"a": None}])
-df_text = pandas.DataFrame([{"a": "hello"}, {"a": "Goodbye"}, {"a": None}])
+df_int = [{"a": 1}, {"a": 2}, {"a": 3}]
+df_float = [{"a": 1.0}, {"a": 2.1}, {"a": 3.0}]
+df_dt = [{"a": today.strftime("%Y-%m-%d %H:%M:%S")}, {"a": today.strftime("%Y-%m-%d %H:%M:%S")}, {"a": None}]
+df_date = [{"a": today_date}, {"a": today_date}, {"a": None}]
+df_bool = [{"a": False}, {"a": True}, {"a": None}]
+df_text = [{"a": "Goodbye"}, {"a": "hello"}, {"a": None}]
+
+
+def dt_stringer(df):
+    return df.astype(str).replace({'NaT': None})
 
 
 @pytest.mark.parametrize(
-    "df",
+    "df,formatter",
     [
-        df_int,
-        df_float,
-        df_dt,
-        df_date,
-        df_bool,
-        df_text,
+        (df_int, None),
+        (df_float, None),
+        (df_dt, dt_stringer),
+        (df_date, None),
+        (df_bool, None),
+        (df_text, None),
     ],
 )
-def test_upload_with_view(df):
-    df = df.copy()
-    df["order_col"] = df.index
-
+def test_upload_with_view(df, formatter):
     interface = upload(
         source=df.copy(),  # needed for the comparison later
         schema_name="public",
@@ -56,9 +57,12 @@ def test_upload_with_view(df):
     )
 
     with interface.get_db_conn() as conn:
-        df_out = pandas.read_sql(f"select * from {interface.schema_name}.{interface.table_name}_view2 order by order_col", conn)
-    assert df.equals(df_out) or (df == df_out).all().iat[0]
+        df_out = pandas.read_sql(f"select * from {interface.schema_name}.{interface.table_name}_view2 order by a", conn)
+    if formatter is not None:
+        df_out = formatter(df_out)
+    df_out = df_out.to_dict("records")
+    assert str(df) == str(df_out)
 
 
 if __name__ == '__main__':
-    test_upload_with_view(df_int)
+    test_upload_with_view(df_dt, dt_stringer)
--- tests/local_utilities/fix_column_types_defined.py
+++ tests/local_utilities/fix_column_types_defined.py
@@ -3,36 +3,37 @@ import pathlib
 sys.path.insert(0, str(pathlib.Path(__file__).parents[2]))
 from redshift_upload import local_utilities  # noqa
 from redshift_upload.db_interfaces import dummy  # noqa
-import pandas  # noqa
 import pytest  # noqa
 
-int_in = pandas.DataFrame([{"a": 10000}, {"a": 20000}, {"a": 30000}])
-int_out1 = pandas.DataFrame([{"a": "10000"}, {"a": "20000"}, {"a": "30000"}])
-int_out2 = pandas.DataFrame([{"a": 10000.0}, {"a": 20000.0}, {"a": 30000.0}])
-int_out3 = pandas.DataFrame([{"a": "1970-01-01 00:00:00.000010"}, {"a": "1970-01-01 00:00:00.000020"}, {"a": "1970-01-01 00:00:00.000030"}])
-int_out4 = pandas.DataFrame([{"a": 10000}, {"a": 20000}, {"a": 30000}])
+int_in = [{"a": 10000}, {"a": 20000}, {"a": 30000}]
+int_out1 = [{"a": "10000"}, {"a": "20000"}, {"a": "30000"}]
+int_out2 = [{"a": 10000.0}, {"a": 20000.0}, {"a": 30000.0}]
+dt_in = [{"a": "1970-01-01 00:00:00"}, {"a": "1970-01-01 00:00:00"}, {"a": "1970-01-01 00:00:00"}]
+int_out4 = [{"a": 10000}, {"a": 20000}, {"a": 30000}]
 
-bool_in = pandas.DataFrame([{"a": "True"}, {"a": False}, {"a": None}])
-bool_out1 = pandas.DataFrame([{"a": "True"}, {"a": "False"}, {"a": None}])
-bool_out2 = pandas.DataFrame([{"a": True}, {"a": False}, {"a": None}])
+bool_in = [{"a": "True"}, {"a": False}, {"a": None}]
+bool_out1 = [{"a": "True"}, {"a": "False"}, {"a": None}]
+bool_out2 = [{"a": True}, {"a": False}, {"a": None}]
 
 
 @pytest.mark.parametrize(
     "df_in,df_out,typ",
     [
-        (int_in.copy(), int_out1, "varchar(10)"),
-        (int_in.copy(), int_out2, "double precision"),
-        (int_in.copy(), int_out3, "timestamp"),
-        (int_in.copy(), int_out4, "bigint"),
-        (bool_in.copy(), bool_out1, "varchar(10)"),
-        (bool_in.copy(), bool_out2, "boolean"),
+        (int_in, int_out1, "VARCHAR"),
+        (int_in, int_out2, "DOUBLE PRECISION"),
+        (dt_in, dt_in, "TIMESTAMP"),
+        (int_in, int_out4, "BIGINT"),
+        (bool_in, bool_out1, "VARCHAR"),
+        (bool_in, bool_out2, "BOOLEAN"),
     ],
 )
 def test_forcible_conversion_type_defined(df_in, df_out, typ):
-    act_df_out, types_out = local_utilities.fix_column_types(df_in, {"a": {"type": typ}}, dummy.Interface(), False)
-    assert types_out == {"a": typ}
-    assert act_df_out.to_csv(None) == df_out.to_csv(None)  # we do this because ultimately, it's the serialization that matters
+    df_in = local_utilities.load_source(df_in)
+    df_in.predefined_columns = {"a": {"type": typ, "suffix": None}}
+    local_utilities.fix_column_types(df_in, dummy.Interface(), False)
+    assert df_in.column_types['a']['type'] == typ
+    # assert act_df_out.to_csv(None) == df_out.to_csv(None)  # we do this because ultimately, it's the serialization that matters
 
 
 if __name__ == '__main__':
-    test_forcible_conversion_type_defined(int_in.copy(), int_out1, "varchar(10)")
+    test_forcible_conversion_type_defined(int_in, int_out1, "VARCHAR")
