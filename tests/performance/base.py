4908dfe296c4859806816d06eccc601581da53b9
Matthew Hamilton <mwhamilton6@gmail.com>
2022-01-04 15:02:28 -0600

    Added more multiprocessing and logging

--- tests/performance/base.py
+++ tests/performance/base.py
@@ -6,6 +6,9 @@ import multiprocessing
 import pandas
 import matplotlib.pyplot as plt
 import seaborn as sns
+import psycopg2.extras
+import time
+import random
 sys.path.insert(0, str(pathlib.Path(__file__).parents[2]))
 from redshift_upload import upload, base_utilities  # noqa
 from redshift_upload.db_interfaces import redshift
@@ -13,7 +16,7 @@ import json  # noqa
 with base_utilities.change_directory():
     with open("../aws_creds.json") as f:
         aws_creds = json.load(f)
-POWERS_CHECKED = 4
+POWERS_CHECKED = 5
 conn = redshift.Interface("public", "perf_test", aws_creds).get_db_conn()
 
 
@@ -30,7 +33,7 @@ def library(data, table_name):
     )
 
 
-def insert(data, table_name):
+def naive_insert(data, table_name):
     insert_query = f'''
     insert into public.{table_name} (a)
     values (%(a)s)
@@ -38,6 +41,15 @@ def insert(data, table_name):
     cursor = conn.cursor()
     cursor.executemany(insert_query, data)
     conn.commit()
+
+
+def batch_insert(data, table_name):
+    insert_query = f'''
+    insert into public.{table_name} (a)
+    values (%(a)s)
+    '''
+    cursor = conn.cursor()
+    psycopg2.extras.execute_batch(cursor, insert_query, data)
 
 
 def setup():
@@ -48,9 +60,9 @@ def setup():
     '''
     cursor = conn.cursor()
     for i in range(POWERS_CHECKED):
-        cursor.execute(query.format(method='library', power=str(i)))
-        cursor.execute(query.format(method='insert', power=str(i)))
-        conn.commit()
+        for method in ['library', 'naive_insert', 'batch_insert']:
+            cursor.execute(query.format(method=method, power=str(i)))
+    conn.commit()
 
 
 def teardown():
@@ -81,25 +93,54 @@ def gen_plot(data):
         fig.savefig("../../documentation/comparison.png", dpi=100)
 
 
-def test_method(method, row_count, version):
+def test_method(args):
+    def pretty(name):
+        name = method.__name__.replace('_', ' ').split()
+        name = [x.capitalize() for x in name]
+        return ' '.join(name)
+
+    method, row_count, version = args
+    print(row_count, method.__name__, 'start')
     data = [{'a': 'hi' * 10} for _ in range(row_count)]
     table_name = f'perf_test_{method.__name__}_{version}'
-    return {
+    if row_count < 10_000:
+        number = 1
+    elif row_count < 100_000:
+        number = 1
+    else:
+        number = 1
+    ret = {
         'rows': row_count,
-        'time': timeit.timeit(functools.partial(method, data, table_name), number=1),
-        'method': method.__name__.capitalize()
+        'time': timeit.timeit(functools.partial(method, data, table_name), number=number),
+        'method': pretty(method)
     }
+    print(row_count, method.__name__, 'finished')
+    return ret
 
 
 def main():
-    results = []
     setup()
-    row_counts = [2 ** (x * 2) for x in range(POWERS_CHECKED)]
-
-    test_method(library, row_counts[0], 0)
+    row_counts = [10 ** x for x in range(POWERS_CHECKED)]
+    library_data = [
+        (library, rows, i)
+        for i, rows in enumerate(row_counts)
+    ]
+    naive_insert_data = [
+        (naive_insert, rows, i)
+        for i, rows in enumerate(row_counts)
+    ]
+    batch_insert_data = [
+        (naive_insert, rows, i)
+        for i, rows in enumerate(row_counts)
+    ]
+    data = library_data + naive_insert_data + batch_insert_data
+    results = []
+    with multiprocessing.Pool(processes=8) as pool:
+        results.extend(pool.map(test_method, data))
     teardown()
 
-    results = pandas.DataFrame(results)
+    results = pandas.DataFrame(results).sort_values("rows")
+    results['rows'] = results['rows'].astype(str)
     gen_plot(results)
 
 
