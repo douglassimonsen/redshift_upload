a22cb1c99a429d9705158b35d1b18345872b9333
Matthew Hamilton <mwhamilton6@gmail.com>
2022-01-10 22:28:37 -0600

    Added ability to run tests from CLI

--- redshift_upload/__main__.py
+++ redshift_upload/__main__.py
@@ -5,6 +5,8 @@ except ModuleNotFoundError:
     from .cli import show_help, show_add_user
     from .cli.gen_environment import gen_environment
 import click
+import pytest
+import os
 
 
 @click.group()
@@ -32,9 +34,17 @@ def add_user():
     show_add_user.main()
 
 
+@click.command()
+def run_tests():
+    "Runs the test suite"
+    os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    pytest.main(["."])
+
+
 cli.add_command(explain_upload_args)
 cli.add_command(help)
 cli.add_command(add_user)
 cli.add_command(gen_environment.gen_environment)
+cli.add_command(run_tests)
 if __name__ == "__main__":
     cli()
