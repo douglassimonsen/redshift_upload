9fc93c58c33989fc14b2a9c91a04b33b362caa97
Matthew Hamilton <mwhamilton6@gmail.com>
2022-01-13 15:31:01 -0600

    Deployments should now happen on new tag

---
+++ setup_deployment.py
@@ -0,0 +1,16 @@
+from distutils.core import run_setup
+import os, sys
+
+this_directory = os.path.dirname(os.path.abspath(__file__)).replace("\\", "/")
+sys.path.insert(0, this_directory)
+from redshift_upload import __version__
+import git
+
+possible_tag = "v" + __version__
+
+repo = git.Repo()
+if possible_tag not in repo.tags:
+    tag = repo.create_tag(
+        possible_tag, message=f"Automatic Tag for release: {possible_tag}"
+    )
+run_setup("setup.py", script_args=["sdist"])
