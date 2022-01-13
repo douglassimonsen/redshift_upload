f2963e591118db6974521719c2074c07ff376308
Matthew Hamilton <mwhamilton6@gmail.com>
2022-01-13 15:37:29 -0600

    handled issue when running setup.py

--- redshift_upload/__init__.py
+++ redshift_upload/__init__.py
@@ -1,6 +1,10 @@
-from .upload import upload
-from . import base_utilities
-from .credential_store import credential_store
+from statistics import mode
 
 
-__version__ = "1.1.4"
+try:
+    from .upload import upload
+    from . import base_utilities
+    from .credential_store import credential_store
+except ModuleNotFoundError:  # needed for when setup.py imports the __version__
+    pass
+__version__ = "1.1.5"
