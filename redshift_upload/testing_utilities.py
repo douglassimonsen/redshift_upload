2b9ca27ff7c327b82fa4353b97a5884db9d0f595
Matthew Hamilton <mwhamilton6@gmail.com>
2022-01-15 17:56:27 -0600

    Added a function to test that the inputs and outputs match. Test upload types now handles more types!

--- redshift_upload/testing_utilities.py
+++ redshift_upload/testing_utilities.py
@@ -1,6 +1,7 @@
 import psycopg2
 import logging
 from typing import List, Dict
+import json
 
 try:
     from .credential_store import credential_store
@@ -9,6 +10,15 @@ except ImportError:
 log = logging.getLogger("redshift_utilities-test")
 
 credential_store.set_store("test-library")
+
+field_types_query = """
+set search_path to {schema_name};
+select "type"
+from pg_table_def
+
+where schemaname = '{schema_name}'
+and tablename = '{table_name}';
+"""
 
 
 def drop_tables(tables: List[str], aws_creds: Dict = None) -> None:
@@ -31,3 +41,45 @@ def drop_tables(tables: List[str], aws_creds: Dict = None) -> None:
             log.info(f"Dropped table: {table}")
         conn.commit()
     log.info("Table dropping completed")
+
+
+def compare_sources(local_data, remote, conn, field_types=None):
+    def stringify(table):
+        table = [{k: str(v) for k, v in row.items()} for row in table]
+        return json.dumps(
+            list(
+                sorted(table, key=lambda x: json.dumps(x, sort_keys=True, default=str))
+            ),
+            sort_keys=True,
+            default=str,
+            indent=2,
+        )
+
+    def get_remote():
+        cursor = conn.cursor()
+        cursor.execute(f"select * from {remote}")
+        rows = cursor.fetchall()
+        columns = [x[0] for x in cursor.description]
+        rows = [dict(zip(columns, row)) for row in rows]
+        return stringify(rows)
+
+    def check_types():
+        cursor = conn.cursor()
+        schema, table = remote.split(".")
+        cursor.execute(field_types_query.format(schema_name=schema, table_name=table))
+        col_types = cursor.fetchall()
+        col_types = [x[0] for x in col_types]
+        assert col_types == field_types
+
+    if field_types is not None:
+        check_types()
+
+    remote_vals = get_remote()
+    local_vals = stringify(local_data)
+    with open("remote", "w") as f:
+        f.write(remote_vals)
+    with open("local", "w") as f:
+        f.write(local_vals)
+
+    # TODO: check column types
+    assert remote_vals == local_vals
--- tests/full_tests/simple/test_upload_types.py
+++ tests/full_tests/simple/test_upload_types.py
@@ -1,6 +1,6 @@
 import pytest
 from redshift_upload import upload, testing_utilities  # noqa
-import pandas  # noqa
+
 
 table_name = (
     "unit_" + __file__.replace("\\", "/").split("/")[-1].split(".")[0]
@@ -11,18 +11,41 @@ table_name = (
 def setup_and_teardown():
     testing_utilities.drop_tables(table_name)
     yield  # this pauses the function for the tests to runa
-    testing_utilities.drop_tables(table_name)
+    # testing_utilities.drop_tables(table_name)
 
 
-df = pandas.DataFrame([{"a": "2020-01-01 04:00"}, {"a": "2020-01-01 04:00"}] * 100)
+df = [
+    {
+        "dt_col": "2020-01-01 04:00:00",
+        "int_col": "1",
+        "time_col": "04:12:34",
+        "var_col": "asd",
+        "date_col": "2020-01-01",
+        "float_col": "1.2",
+    }
+    for _ in range(100)
+]
 
 
 def test_upload_types(schema):
-    upload(
+    interface = upload(
         source=df,
         schema_name=schema,
         table_name=table_name,
-        upload_options={"drop_table": True},
+        upload_options={"close_on_end": False, "drop_table": True},
+    )
+    testing_utilities.compare_sources(
+        df,
+        f"{schema}.{table_name}",
+        interface.get_db_conn(),
+        field_types=[
+            "timestamp without time zone",
+            "smallint",
+            "time without time zone",
+            "character varying(3)",
+            "date",
+            "double precision",
+        ],
     )
 
 
